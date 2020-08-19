1d3d2f9e774500703b6c075d469978231b5c3952
Emma Shih <emmashih721@hotmail.com>
2020-08-19 17:01:42 +0800

    wiring up the provider

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from 'redux'
+
+const songReducer = () => {
+	return [
+		{ title: 'Go Crazy', duration: '3:15' },
+		{ title: 'Wishing Well', duration: '4:07' },
+		{ title: 'Nobodys Love', duration: '2:38' },
+		{ title: 'I Want It in That Way', duration: '1:45' }
+	]
+}
+
+const selectedSongReducer = (selectedSong = null, action) => {
+	if (action.type === 'SONG_SELECTED') {
+		return action.payload
+	}
+
+	return selectedSong
+}
+
+export default combineReducers({
+	song: songReducer,
+	selectedSong: selectedSongReducer
+})
