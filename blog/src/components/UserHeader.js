d7f4b9aeedd63289f3d1f1d0493cfdd769312626
Emma Shih <emmashih721@hotmail.com>
2020-08-23 22:14:46 +0800

    action creators in action creators

--- blog/src/components/UserHeader.js
+++ blog/src/components/UserHeader.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { fetchUser } from '../actions'
 
 export class UserHeader extends Component {
 	componentDidMount() {
-		this.props.fetchUser(this.props.userId)
 	}
 	render() {
 		const { user } = this.props
@@ -27,4 +25,4 @@ const mapStateToProps = (state, ownProps) => {
 	}
 }
 
-export default connect(mapStateToProps, { fetchUser })(UserHeader)
+export default connect(mapStateToProps)(UserHeader)
