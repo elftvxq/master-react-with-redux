6ed25919a82075a48f0dd71ceef8e24736eeaba5
Emma Shih <emmashih721@hotmail.com>
2020-08-23 21:34:54 +0800

    memorize functions by using lodash

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
 export const fetchPosts = () =>
@@ -6,8 +7,12 @@ export const fetchPosts = () =>
 		dispatch({ type: 'FETCH_POSTS', payload: response.data })
 	}
 
-export const fetchUser = (id) =>
-	async dispatch => {
-		const response = await jsonPlaceholder.get(`/users/${id}`)
-		dispatch({ type: 'FETCH_USER', payload: response.data })
-	}
+export const fetchUser = id => async dispatch => {
+	_fetchUser(id, dispatch)
+}
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+	const response = await jsonPlaceholder.get(`/users/${id}`)
+
+	dispatch({ type: 'FETCH_USER', payload: response.data })
+})
