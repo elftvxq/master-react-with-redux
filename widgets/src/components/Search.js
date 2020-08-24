97f808b32c12abc926cab0ccd80f82bb1ef8898a
Emma Shih <emmashih721@hotmail.com>
2020-08-24 17:29:45 +0800

    implementing a delayed request

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const Search = () => {
-	const [term, setTerm] = useState('')
+	const [term, setTerm] = useState('programming')
+	const [results, setResults] = useState([])
+
+	console.log('results: ', results)
 
 	useEffect(() => {
 		const search = async () => {
-			await axios.get('https://en.wikipedia.org/w/api.php', {
+			const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
 				params: {
 					action: 'query',
 					list: 'search',
@@ -15,10 +18,44 @@ const Search = () => {
 					srsearch: term
 				}
 			})
+			setResults(data.query.search)
 		}
 
-		search()
+		if (term && !results.length) {
+			search()
+		} else {
+			const timeoutId = setTimeout(() => {
+				if (term) {
+					search()
+				}
+			}, 1000)
+
+			return () => {
+				clearTimeout(timeoutId)
+			}
+		}
+
 	}, [term])
+
+	const renderedResults = results.map((result => {
+		return (
+			<div key={result.pageid} className="item">
+				<div className="right floated content">
+					<a className="ui button"
+						href={`https://en.wikipedia.org?curid=${result.pageid}}`}
+					>
+						GO
+						</a>
+				</div>
+				<div className="content">
+					<div className="header">
+						{result.title}
+					</div>
+					<span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+				</div>
+			</div>
+		)
+	}))
 
 	return (
 		<div>
@@ -33,6 +70,7 @@ const Search = () => {
 					/>
 				</div>
 			</div>
+			<div className="ui celled list">{renderedResults}</div>
 		</div>
 	)
 }
