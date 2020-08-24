7f2bdea95b845325baf6bf09e2e8c5421095a3f8
Emma Shih <emmashih721@hotmail.com>
2020-08-24 23:28:01 +0800

    handling and showing the option list

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+	const [open, setOpen] = useState(false)
 	const renderedOptions = options.map((option) => {
+		if (option.value === selected.value) {
+			return null
+		}
+
 		return (
 			<div
 				className="item"
 				onClick={() => onSelectedChange(option)}
-				key={option.value}>
+				key={option.value}
+			>
 				{option.lable}
 			</div>
 		)
@@ -15,10 +21,13 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
 		<div className="ui form">
 			<div className="field">
 				<label className="label">Select a Color</label>
-				<div className="ui selection dropdown visible active">
+				<div
+					onClick={() => setOpen(!open)}
+					className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+				>
 					<i className="dropdown icon"></i>
 					<div className="text">{selected.lable}</div>
-					<div className="menu visible transition">
+					<div className={`menu ${open ? 'visible transition' : ''}`}>
 						{renderedOptions}
 					</div>
 				</div>
